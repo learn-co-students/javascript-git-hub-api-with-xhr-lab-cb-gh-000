431c894254725e66b28d33f4a0541050977645eb
Robert Wayne <robertjwayne@gmail.com>
2017-06-09 18:46:47 -0400

    Add getCommits and displayCommits functions

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function displayRepositories(event, data) {
     let repoListItem = '<li>';
     repoListItem += `<a href="${r.html_url}">${r.name}</a>`;
     repoListItem += ' - ';
-    repoListItem += `<a href="#" data-repo="${r.name}" onclick="getCommits(this)">Get Commits</a>`;
+    repoListItem += `<a href="#" data-repository="${r.name}" data-username="${r.owner.login}" onclick="getCommits(this)">Get Commits</a>`;
     repoListItem += ' - ';
-    repoListItem += `<a href="#" data-repo="${r.name}" onclick="getBranches(this)">Get Branches</a>`;
+    repoListItem += `<a href="#" data-repository="${r.name}" data-username="${r.owner.login}" onclick="getBranches(this)">Get Branches</a>`;
     repoListItem += '</li>';
     return repoListItem;
   });
@@ -16,10 +16,34 @@ function displayRepositories(event, data) {
   document.getElementById('repositories').innerHTML = repoListHTML;
 }
 
+function displayCommits(event, data) {
+  const commits = JSON.parse(this.responseText);
+
+  const commitsListItems = commits.map((commit) => {
+    let commitsListItem = '<li>';
+    commitsListItem += `<strong>${commit.author.login}</strong> (${commit.commit.author.name})`;
+    commitsListItem += `- ${commit.commit.message}`;
+    commitsListItem += '</li>';
+    return commitsListItem;
+  });
+
+  const commitsListHTML = `<ul>${commitsListItems.join('')}</ul>`;
+  document.getElementById('details').innerHTML = commitsListHTML;
+}
+
 function getRepositories() {
   const username = document.getElementById('username').value;
   const req = new XMLHttpRequest();
   req.addEventListener('load', displayRepositories);
   req.open('get', `https://api.github.com/users/${username}/repos`);
+  req.send();
+}
+
+function getCommits(element) {
+  const repository = element.dataset.repository;
+  const username = element.dataset.username
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayCommits);
+  req.open('get', `https://api.github.com/repos/${username}/${repository}/commits`);
   req.send();
 }
