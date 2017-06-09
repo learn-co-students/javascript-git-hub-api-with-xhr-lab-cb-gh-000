56642fa307851036c46459887b4aa6f7c183170f
Robert Wayne <robertjwayne@gmail.com>
2017-06-09 17:35:35 -0400

    Add displayRepositories function

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
+function displayRepositories(event, data) {
+  const repos = JSON.parse(this.responseText);
+
+  const repoListItems = repos.map((r) => {
+    let repoListItem = '<li>';
+    repoListItem += `<a href="${r.html_url}">${r.name}</a>`;
+    repoListItem += ' - ';
+    repoListItem += `<a href="#" data-repo="${r.name}" onclick="getCommits(this)">Get Commits</a>`;
+    repoListItem += ' - ';
+    repoListItem += `<a href="#" data-repo="${r.name}" onclick="getBranches(this)">Get Branches</a>`;
+    repoListItem += '</li>';
+    return repoListItem;
+  });
+
+  const repoListHTML = `<ul>${repoListItems.join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = repoListHTML;
+}
+
 function getRepositories() {
   const username = document.getElementById('username').value;
   const req = new XMLHttpRequest();
-  req.addEventListener('load', showRepositories);
+  req.addEventListener('load', displayRepositories);
   req.open('get', `https://api.github.com/users/${username}/repos`);
   req.send();
 }
